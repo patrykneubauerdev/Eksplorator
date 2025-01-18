2ef75a10fa5763afcb5bf81545f7425d42349849
PatrykJanNeubauer <patryk.jan.neubauer@gmail.com>
2025-01-18 16:00:12 +0100

    Initial Commit

---
+++ Eksplorator/EksploratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EksploratorApp.swift
+//  Eksplorator
+//
+//  Created by Patryk Neubauer on 18/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct EksploratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
