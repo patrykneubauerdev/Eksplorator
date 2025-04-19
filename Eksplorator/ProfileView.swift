56b4e5db32cbdbd7fc7fafefe65ea5c9e7670123
Patryk Neubauer <patrykneubauerdev@gmail.com>
2025-04-19 20:16:48 +0200

    Version 1.0.0 Build (2) Release - Minor bugs fixed, added feature whether urbex is active or inactive, adding splash screen and icon

--- Eksplorator/ProfileView.swift
+++ Eksplorator/ProfileView.swift
@@ -216,7 +216,7 @@ struct ProfileView: View {
                                 VStack {
                                     HStack {
                                         Spacer()
-                                        Text("Version 1.0.1")
+                                        Text("Version 1.0.0")
                                             .font(.footnote)
                                             .foregroundStyle(.gray)
                                         
